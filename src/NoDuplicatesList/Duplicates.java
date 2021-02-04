c69806278ed33936d771b11846c732d5d79e46c9
AnastAnton <73544082+AnastAnton@users.noreply.github.com>
2021-02-04 20:54:07 +0200

    .

--- src/NoDuplicatesList/Duplicates.java
+++ src/NoDuplicatesList/Duplicates.java
@@ -8,16 +8,16 @@ public class Duplicates {
     public static void main(String[] args) {
         List<String> colors = new ArrayList<>();
         colors.add("yellow");colors.add("purple");colors.add("brown");
-        colors.add("black");colors.add("black");colors.add("black");colors.add("black");
+        colors.add("black");colors.add("black");
         colors.add("blue");colors.add("brown");
-        colors.add("red");colors.add("red");colors.add("red");colors.add("brown");
+        colors.add("red");colors.add("red");colors.add("brown");
         colors.add("grey");colors.add("grey");
-        colors.add("orange");colors.add("orange");colors.add("brown");colors.add("orange");colors.add("yellow");colors.add("orange");colors.add("orange");
+        colors.add("orange");colors.add("black");colors.add("orange");colors.add("brown");colors.add("orange");colors.add("yellow");colors.add("orange");
         colors.add("violet");colors.add("black");
-        colors.add("lightblue");colors.add("lightblue");colors.add("lightblue");colors.add("yellow");colors.add("lightblue");colors.add("lightblue");colors.add("lightblue");colors.add("lightblue");colors.add("lightblue");        
+        colors.add("lightblue");colors.add("black");colors.add("lightblue");colors.add("lightblue");colors.add("yellow");colors.add("lightblue");colors.add("lightblue");colors.add("orange");colors.add("lightblue");colors.add("lightblue");colors.add("lightblue");        
         colors.add("brown");colors.add("brown");
         colors.add("yellow");
-        colors.add("black");colors.add("black");colors.add("black");
+        colors.add("black");colors.add("black");colors.add("red");colors.add("black");
         
         for(int k = 0; k < colors.size(); k++){
             for (int i = 0; i < colors.size(); i++) {
