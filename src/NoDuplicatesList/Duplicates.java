86361c0fa3a038fb69fd84ad37be9f4911e389a8
AnastAnton <73544082+AnastAnton@users.noreply.github.com>
2021-02-04 20:35:52 +0200

    plus one for loop to play correctly if objects are shuffled

--- src/NoDuplicatesList/Duplicates.java
+++ src/NoDuplicatesList/Duplicates.java
@@ -7,21 +7,24 @@ public class Duplicates {
 
     public static void main(String[] args) {
         List<String> colors = new ArrayList<>();
-        colors.add("purple");
+        colors.add("yellow");colors.add("purple");colors.add("brown");
         colors.add("black");colors.add("black");colors.add("black");colors.add("black");
-        colors.add("blue");
-        colors.add("red");colors.add("red");colors.add("red");
+        colors.add("blue");colors.add("brown");
+        colors.add("red");colors.add("red");colors.add("red");colors.add("brown");
         colors.add("grey");colors.add("grey");
-        colors.add("orange");colors.add("orange");colors.add("orange");colors.add("orange");colors.add("orange");
-        colors.add("violet");
-        colors.add("lightblue");colors.add("lightblue");colors.add("lightblue");colors.add("lightblue");colors.add("lightblue");colors.add("lightblue");colors.add("lightblue");colors.add("lightblue");        
+        colors.add("orange");colors.add("orange");colors.add("brown");colors.add("orange");colors.add("yellow");colors.add("orange");colors.add("orange");
+        colors.add("violet");colors.add("black");
+        colors.add("lightblue");colors.add("lightblue");colors.add("lightblue");colors.add("yellow");colors.add("lightblue");colors.add("lightblue");colors.add("lightblue");colors.add("lightblue");colors.add("lightblue");        
         colors.add("brown");colors.add("brown");
         colors.add("yellow");
+        colors.add("black");colors.add("black");colors.add("black");
         
-        for (int i = 0; i < colors.size(); i++) {
-            for (int j = 0; j < colors.size()-1; j++) {
-                if (colors.get(j).equals(colors.get(j+1))) {
-                    colors.remove(j);
+        for(int k = 0; k < colors.size(); k++){
+            for (int i = 0; i < colors.size(); i++) {
+                for (int j = i+1; j < colors.size(); j++) {
+                    if (colors.get(i).equals(colors.get(j))) {
+                        colors.remove(j);
+                    }
                 }
             }
         }
